914ef30201bdd1f3ddd5f805eece7d1e7217416b
JoramTeneza <joram.teneza@gmail.com>
2023-05-30 11:00:09 +0800

    fix: re layout login and signup page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import { NavLink, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
+import React from "react";
+import LoginSignupForm from "./pages/LoginPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen uppercase">
-      <div className="bg-cyan-500 rounded-md pb-14 px-10 pt-2 text-zinc-50">
-        <div className="flex">
-          <NavLink to={"/"}>Sign In</NavLink>
-          <NavLink to={"/signUp"}>Sign Up</NavLink>
-        </div>
-        <Routes>
-          <Route path="/signUp" element={<SignUpPage />} />
-          <Route path="/" element={<LoginPage />} />
-        </Routes>
-      </div>
+    <div>
+      <LoginSignupForm />
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LoginPage() {
-  return <div className="login-container">Login Page</div>;
-}
+const LoginSignupForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
 
-export default LoginPage;
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (isLogin) {
+      console.log("Logging in...");
+    } else {
+      console.log("Signing up...");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleFormSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-6">
+          {isLogin ? "Login" : "Sign Up"}
+        </h2>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            {isLogin ? "Login" : "Sign Up"}
+          </button>
+
+          <button
+            className="text-blue-500 hover:text-blue-800 font-bold"
+            type="button"
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? "Create an account" : "Already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginSignupForm;
